69a6c603fa9e6854f0e7049bf6078aeb71ebb8b1
Viktor Moyseyenko <vityok@gmail.com>
2021-06-27 20:16:08 +0300

    Create choco_install_all.bat

---
+++ choco_install_all.bat
@@ -0,0 +1,3 @@
+choco install -y git -params '"/GitAndUnixToolsOnPath"'
+choco install -y miniconda3 --params="'/AddToPath=1' /D=C:\conda\ /RegisterPython=1"
+choco install -y winmerge p4v Cmder hyper sourcetree virtualbox wireshark vscode notepadplusplus 7zip putty bat f.lux paint.net astrogrep freemind zerotier-one lightshot licecap autoruns procexp ditto freecommander-xe.install filezilla google-chrome-x64 Firefox 
